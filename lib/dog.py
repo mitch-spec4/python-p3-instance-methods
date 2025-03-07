dd2432a2f5cdc8df0f84b901e699657b1418cd2a
mitch kamau <michchore@gmail.com>
2025-03-07 10:24:48 +0300

    main

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,22 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
+
 
     #Instance method definition
     pass
+
+luna = Dog()
+luna.bark()
+luna.sit()
+
+rex = Dog()
+rex.bark()
+rex.sit()
+
+luna.__dir__()
